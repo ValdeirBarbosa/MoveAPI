96205096b4f5d4163935b57ceb04779d933573ed
valdeirbarbosa <valdeir.barbosa@outlook.com>
2023-06-27 23:31:32 -0300

    ajuste de commit

--- src/controllers/MoviesNotesController.js
+++ src/controllers/MoviesNotesController.js
@@ -1,22 +1,49 @@
+const knex = require("../database/knex")
 const AppError = require("../utils/AppError")
 const sqliteConnection = require("../database/sqlite")
 
 
 class MovieNotesController {
   async create(request, response) {
-    const { title, description, movie_rate } = request.body
+    const { title, description, movie_rate, tags } = request.body
     const { user_id } = request.params
     const database = await sqliteConnection()
 
     const user = await database.get("SELECT * FROM users WHERE id = (?)", [user_id])
-    if(!user){
-      throw new AppError('User not found',404)
-    }else{
-      await database.run("INSERT INTO movies_notes (title, description, movie_rate, user_id) VALUES (?,?,?,?)",
-        [title, description, movie_rate, user_id])
-        return response.json({ user_id })
+
+
+    if (!user) {
+      throw new AppError('User not found', 404)
+    } else {
+
+      const moviesNotes = await knex("movies_notes").insert(
+        {
+          title, description, movie_rate, user_id
+        }
+      )
+
+      const tagsMovie = tags.map(tag => {
+        return {
+          user_id: user_id,
+          tag_name: tag,
+          movie_notes_id: moviesNotes
+
+        }
+      })
+
+
+      await knex("movie_tags").insert(tagsMovie)
+
+      // await database.run("INSERT INTO movies_notes (title, description, movie_rate, user_id) VALUES (?,?,?,?)",
+      //   [title, description, movie_rate, user_id])
+
+
+      return response.json({
+        "status": "SUCCESS",
+        message: `The movie note of the movie ${title} has been created =)`
+      })
     }
-   
+
   }
 }
 
--- src/database/knex/migrations/20230624185637_createMovieTags.js
+++ src/database/knex/migrations/20230624185637_createMovieTags.js
@@ -3,10 +3,8 @@ exports.up = knex => knex.schema.createTable("movie_tags", table => {
 
   table.increments("id");
   table.integer("user_id").references("id").inTable("users");
+  table.integer("tag_name"),
   table.integer("movie_notes_id").references("id").inTable("movies_notes").onDelete("CASCADE")
-  table.timestamp("created_at").default(knex.fn.now());
-  table.timestamp("updated_at").default(knex.fn.now());
-
 })
 
 exports.down = knex => knex.schema.dropTable("movie_tags");
