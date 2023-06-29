e85bdd8271006cabaf62da7096a816b30d87babf
valdeirbarbosa <valdeir.barbosa@outlook.com>
2023-06-28 22:52:28 -0300

    inserindo tags no banco

--- src/controllers/MoviesNotesController.js
+++ src/controllers/MoviesNotesController.js
@@ -7,9 +7,7 @@ class MovieNotesController {
   async create(request, response) {
     const { title, description, movie_rate, tags } = request.body
     const { user_id } = request.params
-    const database = await sqliteConnection()
-
-    const user = await database.get("SELECT * FROM users WHERE id = (?)", [user_id])
+    const user = knex("users").where({ user_id })
 
 
     if (!user) {
@@ -21,22 +19,18 @@ class MovieNotesController {
           title, description, movie_rate, user_id
         }
       )
-
-      const tagsMovie = tags.map(tag => {
+      console.log(typeof(moviesNotes))
+      const listTags = tags.map(tag =>{
         return {
-          user_id: user_id,
-          tag_name: tag,
-          movie_notes_id: moviesNotes
-
+          user_id,
+          tag_name:tag,
+          movie_notes_id: moviesNotes.id
         }
       })
 
+      await knex("movie_tags").insert(listTags)
 
-      await knex("movie_tags").insert(tagsMovie)
-
-      // await database.run("INSERT INTO movies_notes (title, description, movie_rate, user_id) VALUES (?,?,?,?)",
-      //   [title, description, movie_rate, user_id])
-
+     
 
       return response.json({
         "status": "SUCCESS",
