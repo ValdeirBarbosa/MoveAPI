812a9ee85c8e5d1fe02d600aa6f9bb8fb84277e8
Barbosa, Valdeir <barbov1@medtronic.com>
2023-06-29 10:44:57 -0300

    ajuste de rota de criação de movies_notes

--- src/controllers/MoviesNotesController.js
+++ src/controllers/MoviesNotesController.js
@@ -7,31 +7,27 @@ class MovieNotesController {
   async create(request, response) {
     const { title, description, movie_rate, tags } = request.body
     const { user_id } = request.params
-    const user = knex("users").where({ user_id })
-
-
+    // [user] =  get the first index value of array 
+    const [user] = await knex("users").where({ id: user_id })
     if (!user) {
       throw new AppError('User not found', 404)
     } else {
-
-      const moviesNotes = await knex("movies_notes").insert(
+      // [moviesNotes]  get the first index value of array
+      const [moviesNotes] = await knex("movies_notes").insert(
         {
           title, description, movie_rate, user_id
         }
       )
-      console.log(typeof(moviesNotes))
-      const listTags = tags.map(tag =>{
+      console.log(moviesNotes);
+      const listTags = tags.map(tag => {
         return {
           user_id,
-          tag_name:tag,
-          movie_notes_id: moviesNotes.id
+          tag_name: tag,
+          movie_notes_id: moviesNotes
         }
       })
 
       await knex("movie_tags").insert(listTags)
-
-     
-
       return response.json({
         "status": "SUCCESS",
         message: `The movie note of the movie ${title} has been created =)`
--- src/database/knex/migrations/20230624185637_createMovieTags.js
+++ src/database/knex/migrations/20230624185637_createMovieTags.js
@@ -1,10 +1,10 @@
 
 exports.up = knex => knex.schema.createTable("movie_tags", table => {
 
-  table.increments("id");
-  table.integer("user_id").references("id").inTable("users");
-  table.integer("tag_name"),
+  table.increments("id")
+  table.integer("user_id").references("id").inTable("users")
   table.integer("movie_notes_id").references("id").inTable("movies_notes").onDelete("CASCADE")
+  table.integer("tag_name")
 })
 
 exports.down = knex => knex.schema.dropTable("movie_tags");
