28105694c02505a677330a54992a20454f562ed0
Barbosa, Valdeir <barbov1@medtronic.com>
2023-07-05 15:52:19 -0300

    Ajuste de verificaçao de e-mail, aplicando um middleware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,13 +1,20 @@
 const { Router } = require("express");
 
 const UserController = require("../controllers/UsersController");
-
+const ValidateEmail = require('../utils/ValidateEmail')
+const AppError = require('../utils/AppError')
 
 const userRoutes = Router();
 
 
-function myMiddleware(request, response, next) {
-  console.log("YOu pass in the middleware")
+function middlewareValidateEmail(request, response, next) {
+  const { email } = request.body
+  if (!email) {
+    throw new AppError(`The email is required!`,401)
+  }
+  if (!ValidateEmail(email)) {
+    throw new AppError(`The email ${email} is not a valid email!`,401)
+  }
   next()
 
 }
@@ -16,11 +23,11 @@ const userController = new UserController()
 
 
 
-userRoutes.post("/",userController.create)
-userRoutes.get("/",userController.index)
-userRoutes.get("/:user_id",userController.show)
-userRoutes.put("/:id",userController.update)
-userRoutes.delete("/:id",userController.drop)
+userRoutes.post("/", middlewareValidateEmail, userController.create)
+userRoutes.put("/:id", middlewareValidateEmail,userController.update)
+userRoutes.get("/:user_id", userController.show)
+userRoutes.delete("/:id", userController.drop)
+userRoutes.get("/", userController.index)
 
 
 module.exports = userRoutes
